3acad0f82cbfc5428f51219e8795d01f2d08e798
Ying Deng <dengy9@student.douglascollege.ca>
2022-06-19 23:21:11 -0700

    Modified Home page

--- gamersup-frontend/src/App.js
+++ gamersup-frontend/src/App.js
@@ -6,32 +6,35 @@ import SignupForm from './components/account/SignupForm'
 import Footer from './components/layout/Footer'
 import Navbar from './components/layout/Navbar'
 import { AlertProvider } from './context/alert/AlertContext'
+import { GamesProvider } from './context/games/GamesContext'
 import { UserProvider } from './context/user/UserContext'
 import Home from './pages/Home'
 import NotFound from './pages/NotFound'
 
 function App() {
   return (
-    <UserProvider>
-      <AlertProvider>
-        <Router>
-          <div className='flex flex-col justify-between h-screen'>
-            <Navbar />
-            <main className='container mx-auto px-4 pb-15'>
-              <Routes>
-                <Route path='/' element={<Home />} />
-                <Route path='/login' element={<LoginForm />} />
-                <Route path='/signup' element={<SignupForm />} />
-                <Route path='/forgotpassword' element={<ForgotPassword />} />
-                <Route path='/resetpassword' element={<ResetPassword />} />
-                <Route path='/*' element={<NotFound />} />
-              </Routes>
-            </main>
-            <Footer />
-          </div>
-        </Router>
-      </AlertProvider>
-    </UserProvider>
+    <GamesProvider>
+      <UserProvider>
+        <AlertProvider>
+          <Router>
+            <div className='flex flex-col justify-between h-screen'>
+              <Navbar />
+              <main className='container mx-auto px-4 pb-15'>
+                <Routes>
+                  <Route path='/' element={<Home />} />
+                  <Route path='/login' element={<LoginForm />} />
+                  <Route path='/signup' element={<SignupForm />} />
+                  <Route path='/forgotpassword' element={<ForgotPassword />} />
+                  <Route path='/resetpassword' element={<ResetPassword />} />
+                  <Route path='/*' element={<NotFound />} />
+                </Routes>
+              </main>
+              <Footer />
+            </div>
+          </Router>
+        </AlertProvider>
+      </UserProvider>
+    </GamesProvider>
   )
 }
 
--- gamersup-frontend/src/pages/Home.jsx
+++ gamersup-frontend/src/pages/Home.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import GamesList from '../components/games/GamesList'
 
 function Home() {
   return (
-    <div>Home</div>
+    <GamesList />
   )
 }
 
